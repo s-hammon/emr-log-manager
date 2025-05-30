1c17a3ec39f478ce24d6598736ab1901a56bf979
sven <stevethebassguy@gmail.com>
2025-05-29 21:25:27 -0500

    reading schema from file metadata

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
 import os
 
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
 from google.cloud.exceptions import NotFound
 
 
@@ -13,51 +14,57 @@ def main(event, context):
         print(f"Ignoring non-CSV file: {file_name}")
         return
 
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.get_blob(file_name)
+
+    if not blob:
+        print(f"Could not retrieve blob {file_name} from bucket {bucket_name}")
+        return
+
+    table_id = blob.metadata.get("site") if blob.metadata else None
+    if not table_id:
+        print(f"Missing 'site' in metadata for {file_name}")
+        return
+    schema_json = blob.metadata.get("schema") if blob.metadata else None
+    if not schema_json:
+        print(f"No schema defined for table: {table_id}")
+        return
+
+    try:
+        data = json.loads(schema_json)
+    except json.JSONDecodeError as e:
+        print(f"Failed to decode schema JSON: {e}")
+        return
+
+    schema = []
+    for f in data:
+        name = f.get("Name")
+        field_type = f.get("Type")
+        if not name or not field_type:
+            raise ValueError(f"Invalid field in schema metadata: {f}")
+        schema.append(bigquery.SchemaField(name, field_type))
+
     dataset_id = os.environ["BQ_DATASET"]
-    table_id = os.environ["BQ_TABLE"]
     uri = f"gs://{bucket_name}/{file_name}"
 
-    client = bigquery.Client()
-    table_ref = f"{client.project}.{dataset_id}.{table_id}"
+    bq_client = bigquery.Client()
+    table_ref = f"{bq_client.project}.{dataset_id}.{table_id}"
 
     job_config = bigquery.LoadJobConfig(
         source_format=bigquery.SourceFormat.CSV,
         skip_leading_rows=1,
         write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
-        schema=[
-            bigquery.SchemaField("AppointmentID", "INTEGER"),
-            bigquery.SchemaField("Accession", "STRING"),
-            bigquery.SchemaField("AppointmentDate", "DATE"),
-            bigquery.SchemaField("Location", "STRING"),
-            bigquery.SchemaField("PatientMRN", "STRING"),
-            bigquery.SchemaField("PatientLastName", "STRING"),
-            bigquery.SchemaField("PatientFirstName", "STRING"),
-            bigquery.SchemaField("PatientMiddleName", "STRING"),
-            bigquery.SchemaField("Insurance_Plan", "STRING"),
-            bigquery.SchemaField("Insurance_PlanSecondary", "STRING"),
-            bigquery.SchemaField("Insurance_PlanTertiary", "STRING"),
-            bigquery.SchemaField("ReferringPhysicianFirstName", "STRING"),
-            bigquery.SchemaField("ReferringPhysicianLastName", "STRING"),
-            bigquery.SchemaField("Insurance_SubscriberNumber", "STRING"),
-            bigquery.SchemaField("Insurance_SubscriberNumberSecondary", "STRING"),
-            bigquery.SchemaField("Insurance_SubscriberNumberTertiary", "STRING"),
-            bigquery.SchemaField("ExamResultsStatus", "STRING"),
-            bigquery.SchemaField("ExamFinalizedDate", "TIMESTAMP"),
-            bigquery.SchemaField("ExamFinalizedDate_hourofday", "INTEGER"),
-            bigquery.SchemaField("CPTCode", "STRING"),
-            bigquery.SchemaField("CPTDescription", "STRING"),
-            bigquery.SchemaField("Exam_ExamCode", "STRING"),
-            bigquery.SchemaField("ExamDescriptionDisplay", "STRING"),
-        ],
+        schema=schema,
     )
 
     try:
-        client.get_table(table_ref)
+        bq_client.get_table(table_ref)
     except NotFound:
         print(f"Table {table_ref} not found. Creating from file {file_name}...")
         job_config.write_disposition = bigquery.WriteDisposition.WRITE_EMPTY
 
-    load_job = client.load_table_from_uri(uri, table_ref, job_config=job_config)
+    load_job = bq_client.load_table_from_uri(uri, table_ref, job_config=job_config)
     load_job.result()
 
     print(f"Loaded file {file_name} into {table_ref}")
